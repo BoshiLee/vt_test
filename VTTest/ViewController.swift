bbd099c7531f3e0b2f53fe98f4d0e11dbac3e499
Boshi Li <boshi.litw@gmail.com>
2019-12-17 15:19:34 +0800

    Initial Commit

---
+++ VTTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  VTTest
+//
+//  Created by Boshi Li on 2019/12/17.
+//  Copyright © 2019 Boshi Li. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
